d28660481a56b2d866f3489329f05b3e9ce9fd7f
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-02-28 21:42:26 -0500

    Modify Seed.

--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -17,6 +17,8 @@ namespace Sales.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres.")]
         public string Name { get; set; } = null!;
+
+        public int StateId { get; set; }
         public State? State { get; set; }
     }
 }
