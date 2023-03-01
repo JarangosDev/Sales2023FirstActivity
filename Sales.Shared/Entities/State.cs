1759f47e8b3563205abc35af3f8fa8b2c080c51e
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-02-28 21:02:07 -0500

    Implement DataContext for City and States.

--- Sales.Shared/Entities/State.cs
+++ Sales.Shared/Entities/State.cs
@@ -2,17 +2,18 @@
 
 namespace Sales.Shared.Entities
 {
-    public class Country
+    public class State
     {
         public int Id { get; set; }
 
-        [Display(Name = "País")]
+        [Display(Name = "Estado/Departamento")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres.")]
         public string Name { get; set; } = null!;
+        public Country? Country { get; set; }
+        public ICollection<City>? Cities { get; set; }
 
-        public ICollection<State>? States { get; set; }
+        public int CityNumber => Cities == null ? 0 : Cities.Count;
 
-        public int StatesNumber => States == null ? 0 : States.Count;
     }
 }
